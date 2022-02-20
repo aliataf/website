a3c1e564a1624728d186b51a37561ad1c3438d62
Ali Ataf <aliattaf98@gmail.com>
2022-02-20 04:17:46 +0400

    feat: add social icons

---
+++ src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import * as React from 'react';
+
+import UnstyledLink from '@/components/links/UnstyledLink';
+
+const socials = [
+  { href: 'https://www.github.com/aliataf', label: 'Github' },
+  { href: 'https://www.linkedin.com/in/ali-ataf', label: 'Linkedin' },
+  {
+    href: 'https://stackoverflow.com/users/7024690/ali-ataf',
+    label: 'Stackoverflow',
+  },
+  { href: 'https://twitter.com/AliAttaf1', label: 'Twitter' },
+  { href: 'https://aliataf.medium.com', label: 'Medium' },
+  { href: 'https://www.facebook.com/aliataf8', label: 'Facebook' },
+  { href: 'https://wa.me/+971502357409', label: 'Whatsapp' },
+];
+
+export default function Header() {
+  return (
+    <footer className='bg-primary flex gap-x-1 items-center p-6'>
+      {socials.map(({ href, label }) => (
+        <UnstyledLink
+          href={href}
+          key={`${href}${label}`}
+          className='text-white'
+        >
+          <div className='text-red' style={{ color: 'red', fill: 'green' }}>
+            <Image
+              src={`/icons/${label.toLowerCase()}.svg`}
+              alt={label}
+              width='35px'
+              height='35px'
+              className='cursor-pointer hover:text-white'
+            />
+          </div>
+        </UnstyledLink>
+      ))}
+    </footer>
+  );
+}
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import Footer from '@/components/layout/Footer/Footer';
 import Header from '@/components/layout/Header/Header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -8,6 +9,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className='bg-primary text-white'>
       <Header />
       <main>{children}</main>
+      <Footer />
     </div>
   );
 }
