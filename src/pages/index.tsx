d4f18e1b53161f97d0ff869f5cebfd93d3faa6f9
Ali Ataf <aliattaf98@gmail.com>
2022-02-26 23:45:34 +0400

    chore: tailwind and files configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div className="text-3xl text-green-400">Hello2</div>;
+	return <div className="text-3xl text-green-400">Hello2</div>;
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: 'var(--color-primary)',
+					light: 'var(--color-primary-light)',
+				},
+				secondary: 'var(--color-secondary)',
+				accent: 'var(--color-accent)',
+				action: 'var(--color-action)',
+				dark: '#222222',
+			},
+		},
+	},
+	plugins: [],
 };
