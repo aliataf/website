90591719f73bd79814cb8fda341771da65f0141f
Ali Ataf <20990000122@ogrenci.ibu.edu.tr>
2022-02-28 01:48:09 +0400

    feat: add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 
 import '@/styles/globals.css';
 
@@ -9,6 +10,19 @@ import Layout from '@/components/layout/Layout';
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ViewportProvider>
+			<Script
+				src="https://www.googletagmanager.com/gtag/js?id=G-91BH0596YL"
+				strategy="afterInteractive"
+			/>
+			<Script id="google-analytics" strategy="afterInteractive">
+				{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-91BH0596YL');
+        `}
+			</Script>
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
