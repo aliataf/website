79a4a910a336df5e3cd434e0e79edcbfc176d480
Ali Ataf <20990000122@ogrenci.ibu.edu.tr>
2022-06-05 17:37:22 +0400

    chore: enhanec images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,7 +44,7 @@ export default function Home({ myPhoto }: HomeProps) {
 								className={`h-7/12 ${
 									isTabletOrBigger ? 'w-5/12 -translate-y-11' : 'w-11/12'
 								}`}>
-								<Image src={myPhoto.url} alt={myPhoto.alt} width="880px" height="880px" />
+								<Image src={myPhoto.url} alt={myPhoto.alt} width="500px" height="500px" />
 							</div>
 							<BgCircle>
 								<div className="text-2xl font-light">Hello, I am</div>
