a3b31e1945b75a3ace11a66c23431bb214c90f3e
Ali Ataf <aliattaf98@gmail.com>
2022-08-08 22:41:05 +0400

    fix: try fix for assets in gh pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	assetPrefix: '.',
+	basePath: '/website',
 	images: {
 		loader: 'imgix',
 		path: 'https://noop/',
