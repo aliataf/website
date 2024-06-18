3bc09223c281e8e129251b89b6a2aacb21f8f1c8
Ali Ataf <aliattaf98@gmail.com>
2024-06-19 00:13:25 +0400

    feat: add lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const eslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
+const prettierCommand = (filenames) =>
+  `npx prettier ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')} --write`;
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [eslintCommand, prettierCommand],
+  '!(*.js|*.jsx|*.ts|*.tsx)': [prettierCommand],
+};
