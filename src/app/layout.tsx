3c57f113f72073e7c20ed2c8d5ca58eee2578709
Ali Ataf <aliattaf98@gmail.com>
2024-07-06 22:44:26 +0400

    chore: move metadata to separate file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,11 @@
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
+import { customMetadata } from './metadata';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  metadataBase: new URL('https://aliataf.com'),
-  title: {
-    default: 'Ali Ataf',
-    template: '%s | Ali Ataf',
-  },
-  description: "Ali Ataf's website",
-  publisher: 'Ali Ataf',
-  creator: 'Ali Ataf',
-  category: 'Portfolio',
-  twitter: {
-    card: 'summary_large_image',
-    site: '@aliataf_x',
-    creator: '@aliataf_x',
-    images: 'https://example.com/og.png',
-    title: 'My Twitter account',
-    description: 'My Twitter account',
-  },
-  openGraph: {
-    type: 'website',
-    title: 'Ali Ataf',
-    siteName: "Ali Ataf's website",
-    description: "Ali Ataf's website",
-    url: 'https://aliataf.com',
-    emails: 'ali@aliataf.com',
-  },
-  robots: 'index, follow',
-  authors: { name: 'Ali Ataf', url: 'https://aliataf.com' },
-};
+export const metadata = customMetadata;
 
 export default function RootLayout({
   children,
@@ -42,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-primary`}>
         {children}
         <Analytics />
       </body>
---
+++ src/app/metadata.ts
@@ -0,0 +1,31 @@
+import { Metadata } from 'next';
+
+export const customMetadata: Metadata = {
+  metadataBase: new URL('https://aliataf.com'),
+  title: {
+    default: 'Ali Ataf',
+    template: '%s | Ali Ataf',
+  },
+  description: "Ali Ataf's website",
+  publisher: 'Ali Ataf',
+  creator: 'Ali Ataf',
+  category: 'Portfolio',
+  twitter: {
+    card: 'summary_large_image',
+    site: '@aliataf_x',
+    creator: '@aliataf_x',
+    images: 'https://example.com/og.png',
+    title: 'My Twitter account',
+    description: 'My Twitter account',
+  },
+  openGraph: {
+    type: 'website',
+    title: 'Ali Ataf',
+    siteName: "Ali Ataf's website",
+    description: "Ali Ataf's website",
+    url: 'https://aliataf.com',
+    emails: 'ali@aliataf.com',
+  },
+  robots: 'index, follow',
+  authors: { name: 'Ali Ataf', url: 'https://aliataf.com' },
+};
