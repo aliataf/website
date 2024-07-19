bcbd654e61c33ab20a5141c9e1e3480481342738
Ali Ataf <aliattaf98@gmail.com>
2024-07-19 20:05:39 +0400

    feat: add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,48 @@
+import { BgCircle } from '@/components';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'About',
+};
+
+export default function About() {
+  return (
+    <section className="flex justify-between">
+      <BgCircle className="hidden md:block md:w-[44vw] md:h-[44vw] border-[44px] border-accent" />
+      <div className="flex-1 p-4 sm:pt-32">
+        <div>
+          <div className="flex items-center mt-6">
+            <div className="pr-6 text-3xl">About</div>
+            <div className="flex-1 border border-yellow-400" style={{ height: '1px' }}></div>
+          </div>
+          <div className="mt-6 leading-10 text-gray-300 sm:pr-16 sm:mt-4">
+            <p>
+              I am a Senior Frontend Engineer with over 5 years of experience in web development,
+              specializing in building scalable and maintainable web applications. Currently, I am
+              working at{' '}
+              <a
+                href="https://www.tradeling.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-yellow-400 transition-all duration-700 ease-in-out hover:underline"
+              >
+                Tradeling
+              </a>
+              .
+            </p>
+            <p>
+              My journey in coding began in 2012, at the age of 14, with Visual Basic 6.0, where I
+              created numerous programs and games (though none were uploaded to a public
+              repository). Shortly after, I transitioned to JavaScript, which I have been using
+              extensively ever since. My expertise includes technologies such as{' '}
+              <span className="font-bold">
+                React, Next.js, Angular, Vue.js, TypeScript, Tailwind CSS, and Sass
+              </span>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/bg-circle.tsx
+++ src/components/bg-circle.tsx
@@ -1,21 +1,14 @@
-import clsx from 'clsx';
 import { ReactNode } from 'react';
 
 interface BgCircleProps {
   readonly children?: ReactNode;
-  readonly doubleBorder?: boolean;
   readonly className: string;
 }
 
-export function BgCircle({ children, doubleBorder, className }: BgCircleProps) {
+export function BgCircle({ children, className }: BgCircleProps) {
   return (
     <div
-      className={clsx(
-        'bg-primary-light -translate-x-12 justify-center pl-10 md:pl-28 rounded-full',
-        { 'border-[44px] border-accent': doubleBorder },
-        'w-[14rem] md:w-[24rem] h-[14rem] md:h-[24rem]',
-        className,
-      )}
+      className={`bg-primary-light -translate-x-12 justify-center pl-10 md:pl-28 rounded-full w-[14rem] md:w-[24rem] h-[14rem] md:h-[24rem] ${className}`}
     >
       {children}
     </div>
